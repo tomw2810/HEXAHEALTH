f7cf3da7a00345e9ea7ff6324d2f12c6cbaea5ab
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-03-16 21:57:36 +0000

    First for loop added ready to customise

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+goals_diary = {
+    "Name": "Bob"
+    "Favourite Food": "Cheese"
+    "Favourite Drink": "Coconut Water"
+    "Calorie Goal": 2000
+    "Main Sport": "Golf"
+    "Miles Walked Weekly": 20
+}
+
+week_one = input('How many calories have you had today?: ')
+    if week_one(goals_diary) <= 2000
+        return ("fantastic you have beaten the daily target!")
+
+spellClassMage = raw_input("What type of mage? ").upper() 
+
+## Add function name here
+
+for word in userinput:
+            if word in magicSpells: 
+                player[name] = randHealthMage, spellClass, score    
+            else:
+                print("That isn't a listed type!")
