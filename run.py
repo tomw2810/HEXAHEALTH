e0dc7224d7edaa81ced44601071bbbe60715978e
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-03-20 19:26:35 +0000

    if else statements not displaying what is required needs to be changed

--- run.py
+++ run.py
@@ -17,43 +17,45 @@ goals_diary = {
 
 number = int(input("Please enter the amount of meals you had today:"))
 
-if number in goals_diary >= 4:
-    print(f"{number} is not a great number for today")
+if number in goals_diary >= 3:
+    print(f"{number} is not a great number for today\n")
 else:
-    print(f"{number} is what you should be having daily, nice!")
+    print(f"{number} is what you should be having daily, nice!\n")
 
-resting_heartbeat = int(input("Please enter your heartbeat figure:"))
+    resting_heartbeat = int(input("Please enter your heartbeat figure:"))
 
 if resting_heartbeat in goals_diary >= 89:
-    print(f"{resting_heartbeat} is going towards something too high for you")
+    print(f"{resting_heartbeat} is above your daily target\n")
+    print("Please consider exercising more to improve this\n")
 else:
-    print(f"{resting_heartbeat} is not exactly what you want to see!")
+    print(f"{resting_heartbeat} is not exactly what you want to see!\n")
 
 calories = int(input("Please enter your calorie intake for today:"))
 
-if calories in goals_diary >= 2001:
-    print(f"{calories} is exactly what you want to see!")
+if calories in goals_diary <= 2001:
+    print(f"{calories} is exactly what you want to see!\n")
 else:
-    print(f"{calories} is going towards something too high for you")
+    print(f"{calories} is going towards something too high for you\n")
 
-miles_walked = int(input("Please enter the aount of miles walked for today:"))
+miles_walked = int(
+    input("Please enter the aount of miles walked for today:"))
 
-if miles_walked in goals_diary > 4:
-    print(f"{miles_walked} is exactly what you want to see!")
+if miles_walked in goals_diary:
+    print(f"{miles_walked} is exactly what you want to see!\n")
 else:
-    print(f"{miles_walked} has not reached your goal, walk a bit more")
-
+    print(f"{miles_walked} has not reached your goal, walk a bit more\n")
 
 hours_slept = int(input("Please enter the amount of slept last night:"))
 
 if hours_slept in goals_diary <= 6:
-    print(f"{hours_slept} is not exactly what you want to see!")
+    print(f"{hours_slept} is not exactly what you want to see!\n")
 else:
-    print(f"{hours_slept} has exceeded your daily target, nice one!")
+    print(f"{hours_slept} has exceeded your daily target, nice one!\n")
 
 weight = int(input("Please enter the daily end weight:"))
 
 if weight in goals_diary <= 140:
-    print(f"{weight} means you are on track, but remember dont over do it")
+    print(f"{weight} means you are on track, \n")
+    print("But remember, you shouldnt over do it")
 else:
-    print(f"{weight} means you need to stick to your diary better")
+    print(f"{weight} means you need to stick to your diary better\n")
