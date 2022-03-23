4fdb8c5384cb25fffc4f612288a0f77d813feb30
Tom Woodhouse <tomwoodhouse1991@gmail.com>
2022-03-23 22:22:36 +0000

    Diary start function added, but currently has errors

--- run.py
+++ run.py
@@ -11,10 +11,43 @@ goals_diary = {
     "Weight": 140
 }
 
+
+def start_diary():
+    """
+    We want you input the values that are true to yourself. Make sure you
+    are accurate for the best results. This will take you to the first input
+    for the diary you create.
+    """
+
+    global name
+    name = input("Please type your name and hit the enter key:\n")
+
+    while not name.strip():
+        print("Please enter your name to begin the entries\n”)
+        name = input("Please type your name and hit the enter key:\n")
+    else:
+        print("Welcome to HEXAHEALTH your personal health diary\n”)
+        print(“Input your own figures to see how you stack up\n”)
+        print(“We supply you a guidance diary for you to compare to\n”)
+        print(“Ready? Start now\n”)
+
+   restart_diary = True
+
+    while restart_diary:
+        diary_enteries = input("Are you ready to begin? y/n\n")
+
+        if diary_enteries.lower() == "y":
+            print("Lets go\n")
+            break
+        elif diary_enteries.lower() == "n":
+            print("Type 'y' when you are ready to begin\n")
+        else:
+            print("That is not a valid option\n")
+
 # main()
 
-
-number = int(input("Please enter the amount of meals you had today:"))
+while True:
+    number = int(input("Please enter the amount of meals you had today:"))
 
 if number >= ['Number of Meals']:
     print(f"{number} is not a great number for today\n")
@@ -58,3 +91,17 @@ if weight <= ['Weight']:
     print("But remember, you shouldnt over do it")
 else:
     print(f"{weight} means you need to stick to your diary better\n")
+
+
+LEAVE_DIARY = True
+
+while True:
+    exit_diary = input("Are you sure you want to leave? y/n\n")
+
+if exit_diary.lower() == "y":
+    print("Ok remember to come back tomorrow\n")
+    break:
+elif exit_diary.lower() == "n":
+    print("Type 'y' to enter another diary entry\n")
+else:
+    print("That is not a valid option\n")
